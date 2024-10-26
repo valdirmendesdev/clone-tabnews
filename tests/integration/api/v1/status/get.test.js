030ff67bd0743d27a6a8756c3f47f4fa6a0a9ec4
Valdir Mendes <valdir.mfjesus@hotmail.com>
2024-10-25 23:01:18 -0300

    feat: adds `/api/v1/migrations` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,7 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const result = await fetch("http://localhost:3000/api/v1/status");
-  expect(result.status).toBe(200);
-  const responseBody = await result.json();
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+  const responseBody = await response.json();
   expect(responseBody.update_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
